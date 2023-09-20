3495fbe0c0da23300a47c03c043fded62521dff1
tlqdbsrud <141490250+tlqdbsrud@users.noreply.github.com>
2023-09-20 18:28:18 +0900

    [ADD] 스크립트 추가 (-Yun)
    [ADD]
    버튼 스크립트 추가
    내 아이템(인벤토리) 캔버스 추가
    (-Yun)

---
+++ Assets/Scripts/MenuBtn.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuBtn : MonoBehaviour
+{
+    public Button[] menuBtn;
+    public GameObject[] menuCanvas;
+
+    private void Awake()
+    {
+        menuCanvas[0].gameObject.SetActive(false); // 시작 때 캐릭터 정보 내용 비활성화
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < menuBtn.Length; i++)
+        {
+            int buttonIndex = i;
+            menuBtn[i].onClick.AddListener(() => OnMenuButtonClick(buttonIndex)); // 버튼 클릭 
+        }
+    }
+
+    private void OnMenuButtonClick(int menuIndex)
+    {
+        if(menuIndex == 0)
+        {
+            menuCanvas[0].gameObject.SetActive(true); // 캐릭터 정보 내용 활성화
+            if (menuCanvas[0].activeSelf)
+            {
+                menuBtn[0].gameObject.SetActive(false);
+                menuBtn[2].gameObject.SetActive(false);
+                menuBtn[3].gameObject.SetActive(false);
+            }
+        }
+        else if(menuIndex == 1)
+        {
+            menuCanvas[0].gameObject.SetActive(false);
+            if (!menuCanvas[0].activeSelf)
+            {
+                menuBtn[0].gameObject.SetActive(true);
+                menuBtn[2].gameObject.SetActive(true);
+                menuBtn[3].gameObject.SetActive(true);
+            }
+        }
+        else if(menuIndex == 2)
+        {
+
+        }
+    }
+}
